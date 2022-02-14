4068a46dcc55e2827fec07726dcd88355fbea195
Tomoki Morita <tmsongbooks215@gmail.com>
2022-02-14 23:05:29 +0900

    styling app name on header

--- src/layouts/Header.svelte
+++ src/layouts/Header.svelte
@@ -1,17 +1,24 @@
 <script>
   import { base } from "$app/paths";
   import { APP_NAME } from "../constants";
+
+  const [account, repository] = APP_NAME.split("/");
 </script>
 
 <header>
   <nav aria-label="header navigation">
-    <a href="{base}/">{APP_NAME}</a>
+    <a href="{base}/"
+      ><span class="account">{account}/</span><span class="repository"
+        >{repository}</span
+      ></a
+    >
   </nav>
 </header>
 
 <style>
   header {
     background-color: #2e3440;
+    font-family: var(--font-mono);
     position: fixed;
     width: 100%;
   }
@@ -24,11 +31,7 @@
   }
 
   a {
-    align-items: center;
-    border-radius: 0.25rem;
     color: #ddd;
-    display: inline-flex;
-    font-weight: 500;
     padding: 0.75rem 1.5rem;
     text-decoration: none;
   }
@@ -36,5 +39,14 @@
   a:active {
     background-color: #3b4252;
     color: #fff;
+  }
+
+  .account {
+    color: #909aae;
+    font-size: 0.85rem;
+  }
+
+  .repository {
+    font-weight: bold;
   }
 </style>
