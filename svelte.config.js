05a55560dfd62e93a25bafcc6b8298ee181d195d
Tomoki Morita <tmsongbooks215@gmail.com>
2022-03-04 01:46:44 +0900

    add autoprefixer

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
   kit: {
     adapter: adapter(),
     browser: {
