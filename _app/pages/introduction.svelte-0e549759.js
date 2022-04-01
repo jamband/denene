4426fb6bbc5d628b8d5ac3fe0573792c5e601dd1
jamband <jamband@users.noreply.github.com>
2022-04-01 11:36:32 +0000

    deploy: f73138144232d7fd2427fa59240f78121847d308

---
+++ _app/pages/introduction.svelte-0e549759.js
@@ -0,0 +1,15 @@
+import{S as I,i as L,s as S,e as p,g as w,r as b,a as t,A as d,c as h,u as k,n as T,m as P,k as x,f as s,x as g}from"../chunks/index-b09f1b37.js";import{B}from"../chunks/BackToHomeLink-08778d68.js";import{a as A,A as C,b as E}from"../chunks/constants-f1c7d45b.js";import{P as H}from"../chunks/Page-7fa7b1ed.js";import"../chunks/paths-396f020f.js";function M(v){let o,r,c,m,f,n,$,l,_,i,u;return n=new H({props:{title:y}}),i=new B({}),{c(){o=p("meta"),r=p("meta"),c=p("meta"),m=p("meta"),f=w(),b(n.$$.fragment),$=w(),l=p("article"),l.innerHTML=`<header><h1>introduction</h1></header> 
+  <section><h2>1</h2> 
+    <p>this site does not use cookies. it uses html and css, and a little
+      javascript. it&#39;s an experimental site to think about what we can do in
+      such a situation.</p></section> 
+  <section><h2>2</h2> 
+    <p>as much as possible, this site does not use anything that works only with
+      pointing devices. e.g. such as <code>hover</code> and <code>cursor</code>.
+      this is an experiment on accessibility.</p></section> 
+  <section><h2>3</h2> 
+    <p>this site basically consists only of lowercase letters and symbols, does
+      not use uppercase letters. this is an experiment on information overload.</p></section> 
+  <section><h2>4</h2> 
+    <p>this site also includes features that do not work on certain browsers. for
+      details, see browser support.</p></section>`,_=w(),b(i.$$.fragment),t(o,"name","description"),t(o,"content",A),t(r,"property","og:title"),t(r,"content",y+" \uFF65 "+C),t(c,"property","og:description"),t(c,"content",A),t(m,"property","og:url"),t(m,"content",E+y+"/")},m(e,a){d(document.head,o),d(document.head,r),d(document.head,c),d(document.head,m),h(e,f,a),k(n,e,a),h(e,$,a),h(e,l,a),h(e,_,a),k(i,e,a),u=!0},p:T,i(e){u||(P(n.$$.fragment,e),P(i.$$.fragment,e),u=!0)},o(e){x(n.$$.fragment,e),x(i.$$.fragment,e),u=!1},d(e){s(o),s(r),s(c),s(m),e&&s(f),g(n,e),e&&s($),e&&s(l),e&&s(_),g(i,e)}}}const y="introduction";class O extends I{constructor(o){super();L(this,o,null,M,S,{})}}export{O as default};
