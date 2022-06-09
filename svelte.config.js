bd8065044959bcec225487b435525950cc38f6ee
Tomoki Morita <tmsongbooks215@gmail.com>
2022-06-10 02:01:51 +0900

    use double quotation

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import preprocess from "svelte-preprocess";
 
 const dev = process.env.NODE_ENV === "development";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
   preprocess: [
     preprocess({
