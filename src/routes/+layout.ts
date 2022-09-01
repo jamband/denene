9d02684300c756610a9360649e527cf29f4ce07c
Tomoki Morita <tmsongbooks215@gmail.com>
2022-09-01 23:14:26 +0900

    add some types

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,5 @@
 import { dev } from "$app/environment";
+import type { PrerenderOption } from "@sveltejs/kit";
 
 export const csr = dev ? true : false;
-export const prerender = true;
+export const prerender: PrerenderOption = true;
