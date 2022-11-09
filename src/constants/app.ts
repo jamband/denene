56c8b1f52ffe9f946104f280e2cb2e7d91493349
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-09 23:43:39 +0900

    remove unnecessary some constants

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,4 +1,3 @@
 export const APP_NAME = "jamband/denene" as const;
 export const APP_DESCRIPTION = "denene is my private playground for html and css" as const;
-export const APP_URL = "https://jamband.github.io/denene/" as const;
 export const APP_REPOSITORY_URL = `https://github.com/${APP_NAME}` as const;
