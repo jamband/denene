d4ca25b9e5ea42a1a122883bfd6dd6657e5ef27e
Tomoki Morita <tmsongbooks215@gmail.com>
2023-05-22 18:59:15 +0900

    use compressHTML instead of html-minify integrations

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,13 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import htmlMinify from "./integrations/html-minify";
 
 export default defineConfig({
   site: "https://jamband.github.io/",
   base: "/denene",
   trailingSlash: "always",
   scopedStyleStrategy: "class",
-  integrations: [htmlMinify(), sitemap()],
+  compressHTML: true,
+  integrations: [sitemap()],
   vite: {
     build: {
       rollupOptions: {
