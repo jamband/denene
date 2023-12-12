57e4e995e4c26c41ab0203ebb11c813e9b8a4ca6
Tomoki Morita <tmsongbooks215@gmail.com>
2023-12-12 17:55:56 +0900

    disable devToolbar

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   base: "denene/",
   trailingSlash: "always",
   integrations: [sitemap()],
+  devToolbar: { enabled: false },
   vite: {
     build: {
       assetsInlineLimit: 2048,
