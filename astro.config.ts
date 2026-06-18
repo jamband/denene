3840b9c0f7ddb2faf80a12589c51d2e6abaa10a3
Tomoki Morita <tmsongbooks215@gmail.com>
2026-06-18 15:51:24 +0900

    use ubuntu font and some style changes

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
   site: "https://jamband.github.io/",
@@ -7,6 +7,22 @@ export default defineConfig({
   trailingSlash: "always",
   integrations: [sitemap()],
   devToolbar: { enabled: false },
+  fonts: [
+    {
+      name: "Ubuntu",
+      cssVariable: "--font-ubuntu",
+      provider: fontProviders.google(),
+      weights: [400],
+      styles: ["normal"],
+    },
+    {
+      name: "Ubuntu Mono",
+      cssVariable: "--font-ubuntu-mono",
+      provider: fontProviders.google(),
+      weights: [400],
+      styles: ["normal"],
+    },
+  ],
   vite: {
     build: {
       assetsInlineLimit: 2048,
